59e68463d0619c10e236a7d314c4dab85f145d6c
Dennis Gurzhiy <denver.zp.2012@yandex.ua>
2019-09-24 17:16:54 +0300

    init

---
+++ database/migrations/2019_09_24_133939_create_adverts.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAdverts extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('adverts', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('user_id');
+            $table->string('title');
+            $table->text('description');
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('adverts');
+    }
+}
---
+++ readme.md
@@ -0,0 +1,10 @@
+# Redwerk test
+
+Project URL: http://localhost:8000
+
+## Quickstart
+
+1. Run local server ```php artisan serve```
+2. Open project URL in browser ```http://localhost:8000```
+
+
